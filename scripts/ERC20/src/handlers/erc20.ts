4480f75b6aa5b5cb70d25c81b56e2bbc810c863b
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2023-11-29 16:29:09 +0530

    erc20 minor updates

--- scripts/ERC20/src/handlers/erc20.ts
+++ scripts/ERC20/src/handlers/erc20.ts
@@ -1,80 +1,81 @@
 import BigNumber from "bignumber.js";
-/**
- * @dev Event::Approval(address src, address guy, uint256 wad)
- * @param instance database [key, value]
- * @param context trigger object with contains [event: [src ,guy ,wad ], block, transaction]
- */
-export const ApprovalHandler = (db: any, context: any) => {
-  // args: [arg1, arg2, arg3] = [src, guy, wad]
+// /**
+//  * @dev Event::Approval(address src, address guy, uint256 wad)
+//  * @param instance database [key, value]
+//  * @param context trigger object with contains [event: [owner ,spender ,value ], block, transaction]
+//  */
+// export const ApprovalHandler = (db: any, context: any) => {
 
-  // To init a variable in database instance
-  if (!db["approvals"]) db["approvals"] = {};
-  if (!db["approvals"][context.event["arg1"]]) db["approvals"][context.event["arg1"]] = {};
-  if (!db["approvals"][context.event["arg2"]]) db["approvals"][context.event["arg2"]] = {};
+//   // To init a variable in database instance
+//   if (!db["approvals"]) db["approvals"] = {};
+//   if (!db["approvals"][context.event.owner]) db["approvals"][context.event.owner] = {};
+//   if (!db["approvals"][context.event.spender]) db["approvals"][context.event.spender] = {};
 
-  // Initialise the owner -> spender mapping
-  if (!db["approvals"][context.event["arg1"]][context.event["arg2"]])
-    db["approvals"][context.event["arg1"]][context.event["arg2"]] = new BigNumber(0).toString();
+//   // Initialise the owner -> spender mapping
+//   if (!db["approvals"][context.event.owner][context.event.spender])
+//     db["approvals"][context.event.owner][context.event.spender] = new BigNumber(0).toString();
 
-  // Initialise the spender -> owner mapping
-  if (!db["approvals"][context.event["arg2"]][context.event["arg1"]])
-    db["approvals"][context.event["arg2"]][context.event["arg1"]] = new BigNumber(0).toString();
+//   // Initialise the spender -> owner mapping
+//   if (!db["approvals"][context.event.spender][context.event.owner])
+//     db["approvals"][context.event.spender][context.event.owner] = new BigNumber(0).toString();
 
-  // To get variable in database instance
-  let owner = db["approvals"][context.event["arg1"]];
-  let spender = db["approvals"][context.event["arg2"]];
+//   // To get variable in database instance
+//   let owner = db["approvals"][context.event.owner];
+//   let spender = db["approvals"][context.event.spender];
 
-  // Implement your event handler logic for Approval here
-  const zeroAddress = "0x0000000000000000000000000000000000000000";
-  if (context.event["arg1"] !== zeroAddress && context.event["arg2"] !== zeroAddress) {
-    let value = new BigNumber(context.event["arg3"] || 0).toString();
-    owner[context.event["arg2"]] = value; //  How much allowance owner has given to a particular spender
-    spender[context.event["arg1"]] = value; // How much allowance spender has by particular owner
-  }
+//   // Implement your event handler logic for Approval here
+//   const zeroAddress = "0x0000000000000000000000000000000000000000";
+//   if (context.event.owner !== zeroAddress && context.event.spender !== zeroAddress) {
+//     let value = new BigNumber(context.event.value || 0).toString();
+//     owner[context.event.spender] = value; //  How much allowance owner has given to a particular spender
+//     spender[context.event.owner] = value; // How much allowance spender has by particular owner
+//   }
 
-  // To update a variable in database instance
-  db["approvals"][context.event["arg1"]] = owner;
-  db["approvals"][context.event["arg2"]] = spender;
-};
+//   // To update a variable in database instance
+//   db["approvals"][context.event.owner] = owner;
+//   db["approvals"][context.event.spender] = spender;
+// };
 
 /**
  * @dev Event::Transfer(address src, address dst, uint256 wad)
  * @param instance database [key, value]
- * @param context trigger object with contains [event: [src ,guy ,wad ], block, transaction]
+ * @param context trigger object with contains [event: [from ,to ,value ], block, transaction]
  */
 export const TransferHandler = (db: any, context: any) => {
-  // args: [arg1, arg2, arg3] = [src, dst, wad]
 
   // To init a variable in database instance
   if (!db["totalSupply"]) db["totalSupply"] = "0";
   if (!db["balances"]) db["balances"] = {};
 
   // To get variable in database instance
-  let totalSupply = new BigNumber(db["totalSupply"] || 0);
+  let totalSupply = new BigNumber(db["totalSupply"]);
   let balances = db["balances"];
 
   // Implement your event handler logic for Transfer here
   const zeroAddress = "0x0000000000000000000000000000000000000000";
-  if (context.event["arg1"] === zeroAddress) {
-    let toBalance = new BigNumber(balances[context.event["arg2"]] || 0);
-    let value = new BigNumber(context.event["arg3"] || 0);
+  if (context.event.from === zeroAddress) {
+    // mint
+    let toBalance = new BigNumber(balances[context.event.to]);
+    let value = new BigNumber(context.event.value);
     toBalance = toBalance.plus(value);
     totalSupply = totalSupply.plus(value);
-    balances[context.event["arg2"]] = toBalance.toString();
-  } else if (context.event["arg2"] === zeroAddress) {
-    let fromBalance = new BigNumber(balances[context.event["arg1"]] || 0);
-    let value = new BigNumber(context.event["arg3"] || 0);
+    balances[context.event.to] = toBalance.toString();
+  } else if (context.event.to === zeroAddress) {
+    // burn
+    let fromBalance = new BigNumber(balances[context.event.from] || 0);
+    let value = new BigNumber(context.event.value || 0);
     fromBalance = fromBalance.minus(value);
     totalSupply = totalSupply.minus(value);
-    balances[context.event["arg1"]] = fromBalance.toString();
+    balances[context.event.from] = fromBalance.toString();
   } else {
-    let fromBalance = new BigNumber(balances[context.event["arg1"]] || 0);
-    let toBalance = new BigNumber(balances[context.event["arg2"]] || 0);
-    let value = new BigNumber(context.event["arg3"] || 0);
+    // transfer
+    let fromBalance = new BigNumber(balances[context.event.from] || 0);
+    let toBalance = new BigNumber(balances[context.event.to] || 0);
+    let value = new BigNumber(context.event.value || 0);
     fromBalance = fromBalance.minus(value);
     toBalance = toBalance.plus(value);
-    balances[context.event["arg1"]] = fromBalance.toString();
-    balances[context.event["arg2"]] = toBalance.toString();
+    balances[context.event.from] = fromBalance.toString();
+    balances[context.event.to] = toBalance.toString();
   }
 
   // To update a variable in database instance
