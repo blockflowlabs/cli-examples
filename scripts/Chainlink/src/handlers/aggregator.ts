dca3e86de5ee6c2d20debf2c3764c863419ca4af
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2023-11-29 15:21:39 +0530

    chainlink price feed updates

---
+++ scripts/Chainlink/src/handlers/aggregator.ts
@@ -0,0 +1,32 @@
+/**
+ * @dev Event::AnswerUpdated(int256 current, uint256 roundId, uint256 updatedAt)
+ * @param instance database [key, value]
+ * @param context trigger object with contains [event: [current ,roundId ,updatedAt ], block, transaction]
+ */
+export const AnswerUpdatedHandler = (db: any, context: any) => {
+  const aggregatorToTokenSymbol = {
+    "0xE62B71cf983019BFf55bC83B48601ce8419650CC": "ETH",
+    "0xdBe1941BFbe4410D6865b9b7078e0b49af144D2d": "BTC",
+    "0x478238a1c8B862498c74D0647329Aef9ea6819Ed": "DAI",
+    "0xa964273552C1dBa201f5f000215F5BD5576e8f93": "USDT",
+    "0x789190466E21a8b78b8027866CBBDc151542A26C": "USDC"
+  }
+
+  const aggregatorToDecimals = {
+    "0xE62B71cf983019BFf55bC83B48601ce8419650CC": "8",
+    "0xdBe1941BFbe4410D6865b9b7078e0b49af144D2d": "8",
+    "0x478238a1c8B862498c74D0647329Aef9ea6819Ed": "8",
+    "0xa964273552C1dBa201f5f000215F5BD5576e8f93": "8",
+    "0x789190466E21a8b78b8027866CBBDc151542A26C": "8"
+  }
+
+  const aggregatorAddr = context.log.log_address;
+  const tokenSymbol = aggregatorToTokenSymbol[aggregatorAddr];
+  const decimals = aggregatorToDecimals[aggregatorAddr];
+  
+  // To init a variable in database instance
+  if (!db[tokenSymbol]) db[tokenSymbol] = "0";
+
+  // To update a variable in database instance
+  db[tokenSymbol] = (parseInt(context.event.current) / (10 ** parseInt(decimals))).toString();
+};
