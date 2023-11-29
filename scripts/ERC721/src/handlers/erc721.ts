e16a77ba40c1c9488ba3cc52d53705f87229ca17
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2023-11-29 17:04:55 +0530

    erc721: Minor updates

--- scripts/ERC721/src/handlers/erc721.ts
+++ scripts/ERC721/src/handlers/erc721.ts
@@ -1,49 +1,4 @@
 /**
- * @dev Event::Approval(address owner, address approved, uint256 tokenId)
- * @param instance database [key, value]
- * @param event trigger object with keys [owner ,approved ,tokenId ]
- */
-export const ApprovalHandler = (db: any, event: any) => {
-  // To init a variable in database instance
-  // if(!db['from']) db['from'] = {}
-  // To get variable in database instance
-  // let from = db['from']
-  // To update a variable in database instance
-  // db['from'] = event.from || event.arg0 || event['0']
-  // Implement your event handler logic for Approval here
-};
-
-/**
- * @dev Event::ApprovalForAll(address owner, address operator, bool approved)
- * @param instance database [key, value]
- * @param event trigger object with keys [owner ,operator ,approved ]
- */
-export const ApprovalForAllHandler = (db: any, event: any) => {
-  // To init a variable in database instance
-  // if(!db['from']) db['from'] = {}
-  // To get variable in database instance
-  // let from = db['from']
-  // To update a variable in database instance
-  // db['from'] = event.from || event.arg0 || event['0']
-  // Implement your event handler logic for ApprovalForAll here
-};
-
-/**
- * @dev Event::OwnershipTransferred(address previousOwner, address newOwner)
- * @param instance database [key, value]
- * @param event trigger object with keys [previousOwner ,newOwner ]
- */
-export const OwnershipTransferredHandler = (db: any, event: any) => {
-  // To init a variable in database instance
-  // if(!db['from']) db['from'] = {}
-  // To get variable in database instance
-  // let from = db['from']
-  // To update a variable in database instance
-  // db['from'] = event.from || event.arg0 || event['0']
-  // Implement your event handler logic for OwnershipTransferred here
-};
-
-/**
  * @dev Event::Transfer(address from, address to, uint256 tokenId)
  * @param instance database [key, value]
  * @param event trigger object with keys [from ,to ,tokenId ]
