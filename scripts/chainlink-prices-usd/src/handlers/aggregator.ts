99e543ec904ef12cc9b5a4d80bdd08348368a46a
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2023-12-02 15:41:42 +0530

    chainlink apis

--- scripts/chainlink-prices-usd/src/handlers/aggregator.ts
+++ scripts/chainlink-prices-usd/src/handlers/aggregator.ts
@@ -11,24 +11,26 @@ export const AnswerUpdatedHandler = (db: any, context: any) => {
     "0xdBe1941BFbe4410D6865b9b7078e0b49af144D2d": "BTC",
     "0x478238a1c8B862498c74D0647329Aef9ea6819Ed": "DAI",
     "0xa964273552C1dBa201f5f000215F5BD5576e8f93": "USDT",
-    "0x789190466E21a8b78b8027866CBBDc151542A26C": "USDC"
-  }
+    "0x789190466E21a8b78b8027866CBBDc151542A26C": "USDC",
+  };
 
   const aggregatorToDecimals = {
     "0xE62B71cf983019BFf55bC83B48601ce8419650CC": "8",
     "0xdBe1941BFbe4410D6865b9b7078e0b49af144D2d": "8",
     "0x478238a1c8B862498c74D0647329Aef9ea6819Ed": "8",
     "0xa964273552C1dBa201f5f000215F5BD5576e8f93": "8",
-    "0x789190466E21a8b78b8027866CBBDc151542A26C": "8"
-  }
+    "0x789190466E21a8b78b8027866CBBDc151542A26C": "8",
+  };
 
   const aggregatorAddr = context.log.log_address;
   const tokenSymbol = aggregatorToTokenSymbol[aggregatorAddr];
   const decimals = aggregatorToDecimals[aggregatorAddr];
-  
+
   // To init a variable in database instance
   if (!db[tokenSymbol]) db[tokenSymbol] = "0";
 
   // To update a variable in database instance
-  db[tokenSymbol] = new BigNumber(context.event.current).div(new BigNumber(10).pow(decimals)).toString();
+  db[tokenSymbol] = new BigNumber(context.event.current)
+    .div(new BigNumber(10).pow(decimals))
+    .toString();
 };
