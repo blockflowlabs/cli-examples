e7730b522e0881dbaa9538da136a9291f7c4b80c
Vaibhav Khanna <vaibhavkhanna78@gmail.com>
2023-12-26 18:13:14 +0530

    initial setup

---
+++ erc4337/src/handlers/entrypoint/handleOps.ts
@@ -0,0 +1,9 @@
+
+/**
+ * @dev Function::handleOps(tuple[] ops, address beneficiary)
+ * @param context trigger object with contains {function: {ops ,beneficiary }, transaction, block, log}
+ */
+export const handleOpsHandler = (context: any, load: any, save: any) => {
+  // Implement your function handler logic for handleOps here
+
+};
