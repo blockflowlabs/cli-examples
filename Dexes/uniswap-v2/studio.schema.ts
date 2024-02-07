f9f7dfbb973c29063378de4a3ac06e080488cce8
pradyuman-verma <pradyumnverma27@gmail.com>
2024-02-07 20:03:23 +0530

    more types

--- Dexes/uniswap-v2/studio.schema.ts
+++ Dexes/uniswap-v2/studio.schema.ts
@@ -68,3 +68,120 @@ interface Bundle {
   id: String;
   ethPrice: String;
 }
+
+interface Transaction {
+  id: String;
+  timestamp: String;
+  mints: [String];
+  burns: [String];
+  swaps: [String];
+}
+
+interface Mint {
+  id: String;
+  transaction: String;
+  timestamp: String;
+  pair: String;
+  to: String;
+  liquidity: String;
+  sender: String;
+  amount0: String;
+  amount1: String;
+  logIndex: String;
+  amountUSD: String;
+  feeTo: String;
+  feeLiquidity: String;
+}
+
+interface LiquidityPosition {
+  id: String;
+  user: String;
+  pair: String;
+  liquidityTokenBalance: String;
+}
+
+interface LiquidityPositionSnapshot {
+  id: String;
+  liquidityPosition: String;
+  timestamp: String;
+  user: String;
+  pair: String;
+  token0PriceUSD: String;
+  token1PriceUSD: String;
+  reserve0: String;
+  reserve1: String;
+  reserveUSD: String;
+  liquidityTokenTotalSupply: String;
+  liquidityTokenBalance: String;
+}
+
+interface PairDayData {
+  id: String;
+  date: String;
+  pairAddress: String;
+  token0: String;
+  token1: String;
+
+  reserve0: String;
+  reserve1: String;
+
+  totalSupply: String;
+
+  reserveUSD: String;
+
+  dailyVolumeToken0: String;
+  dailyVolumeToken1: String;
+  dailyVolumeUSD: String;
+  dailyTxns: String;
+}
+
+interface PairHourData {
+  id: String;
+  hourStartUnix: String;
+  pair: String;
+
+  reserve0: String;
+  reserve1: String;
+
+  totalSupply: String;
+
+  reserveUSD: String;
+
+  hourlyVolumeToken0: String;
+  hourlyVolumeToken1: String;
+  hourlyVolumeUSD: String;
+  hourlyTxns: String;
+}
+
+interface UniswapDayData {
+  id: String;
+  date: String;
+
+  dailyVolumeETH: String;
+  dailyVolumeUSD: String;
+  dailyVolumeUntracked: String;
+
+  totalVolumeETH: String;
+  totalLiquidityETH: String;
+  totalVolumeUSD: String;
+  totalLiquidityUSD: String;
+
+  txCount: String;
+}
+
+interface TokenDayData {
+  id: String;
+  date: String;
+  token: String;
+
+  dailyVolumeToken: String;
+  dailyVolumeETH: String;
+  dailyVolumeUSD: String;
+  dailyTxns: String;
+
+  totalLiquidityToken: String;
+  totalLiquidityETH: String;
+  totalLiquidityUSD: String;
+
+  priceUSD: String;
+}
