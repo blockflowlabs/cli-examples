e4a967b4e94b2cd4bc96e23d5a1ac82b3fdab652
pradyuman-verma <pradyumnverma27@gmail.com>
2024-05-30 19:31:37 +0530

    fixes + updates

--- cctp/src/utils/helper.ts
+++ cctp/src/utils/helper.ts
@@ -1,12 +1,17 @@
-export const sourceDomainMap: { [key: string]: string } = {
-  '0': 'Ethereum',
-  '1': 'Avalanche',
-  '2': 'OP Mainnet',
-  '3': 'Arbitrum',
-  '6': 'Base',
-  '7': 'Polygon PoS',
+// supported domain information - https://developers.circle.com/stablecoins/docs/supported-domains
+
+export const domainToChainId: { [key: string]: string } = {
+  '0': '1', // ethereum
+  '1': '43114', // avalanche
+  '2': '10', // op
+  '3': '42161', // arb
+  '6': '8453', // base
+  '7': '137', // polygon
 }
 
-export function getBlockchainName(chainIdIndex: string): string {
-  return sourceDomainMap[chainIdIndex] ?? 'Unknown Blockchain'
-}
+export const chainIdToDomain: { [key: string]: string } = Object.entries(
+  domainToChainId,
+).reduce<{ [key: string]: string }>((acc, [key, value]) => {
+  acc[value] = key
+  return acc
+}, {})
